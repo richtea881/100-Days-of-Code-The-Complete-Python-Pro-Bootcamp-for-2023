4262d8facf259458479e58fb570b8cd524b07600
richtea881 <doctorskull.z@gmail.com>
2022-12-09 19:40:18 +0100

    Day 2/Exercise 1- Data Types

---
+++ Day1/input_1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+#Write a program that switches the values stored in the variables a and b.
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+
+#create a third variable amd put the content of variable a in it in order to make it easier to swap the value of a and b
+#Write your code below this line 👇
+#third variable is c
+c = a
+a = b
+b = c
+
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
---
+++ Day2/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+#Get the first and second digits using subscripting then convert string to int.
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+
+#Add the two digits together
+two_digit_number = first_digit + second_digit
+print(two_digit_number)
