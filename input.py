592cc53516197d05a3dfd2e4c07049c42d3b1c10
richtea881 <doctorskull.z@gmail.com>
2022-12-09 17:51:16 +0100

    Day 1/Exercise 3 - Input Function

---
+++ input.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+#input() will get user input from the console
+#print() will print the word Hello then the user input
+# Write your code below this line 👇
+print('Hello ' + input("What is your name ?") + "!")
